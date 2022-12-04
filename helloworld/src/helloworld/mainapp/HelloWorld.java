18248cf58de1ab8a4877315299c7c431b540a8b0
prabhat1510 <prabhatc@gmail.com>
2022-12-04 12:51:40 +0530

    helloworld program added

---
+++ helloworld/src/helloworld/mainapp/HelloWorld.java
@@ -0,0 +1,11 @@
+//First line in the java program is Name of the package
+package helloworld.mainapp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Hello Everyone");
+	}
+
+}

b4e62d46082642a4bf65947e41751ab64cd26ba2
prabhat1510 <prabhatc@gmail.com>
2022-12-18 13:34:44 +0530

    Steps to create an EC2 instance in AWS

---
+++ hrmsapp/src/test/java/com/staragile/hrmsapp/service/EmployeeServiceImplTests.java
@@ -0,0 +1,24 @@
+package com.staragile.hrmsapp.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmployeeServiceImplTests {
+	
+	@Test
+	void deleteEmployeeById(){
+		//Test assertions
+		assertThat(true);
+	}
+	
+	@Test
+	void deleteEmployeeByIdFailure(){
+		//Test assertions
+		assertThat(true);
+	}
+	
+	
+}
